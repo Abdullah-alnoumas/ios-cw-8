0d01afc6bd96aa0c082bb281349b6a4e75ce3e28
Abdullah-alnoumas <70814254+Abdullah-alnoumas@users.noreply.github.com>
2022-03-12 10:56:30 +0300

    Hi

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -1,5 +1,26 @@
 import UIKit
 
-// التمرين الأول
-// ClassWork 1
+var name = "Abdullah"
+
+var birthdayYear = 2004
+
+var currentYear = 2022
+
+var age = currentYear-birthdayYear
+
+print("Hello my name is \(name) and my age is \(age)")
+
+
+var num1 = "8"
+
+var num2 = 4
+
+var sum12 = Int(num1)!+num2
+
+var num3 = 2.5
+
+num3 =  Double(num1)! + Double(num2)
+
+//Done
+
 
--- CW4.playground/Contents.swift
+++ CW4.playground/Contents.swift
@@ -1,5 +1,12 @@
 import UIKit
 
-// التمرين الرابع
-// ClassWork 4
 
+var name = "Abdullah"
+
+var birthdayYear = 2004
+
+var currentYear = 2022
+
+var age = 17
+
+print("Hello my name is \(name) and my age is \()")
